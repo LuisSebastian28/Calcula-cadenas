69e63402b4bd043e568d55d2928562b6ee8fc357
sebastian <sabestech@gmail.com>
2022-03-27 17:24:47 -0400

    cadena con dos numeros

--- src/calcularCad.js
+++ src/calcularCad.js
@@ -1,5 +1,14 @@
 function cadena(palabra){
     var num = parseInt(palabra);
+    const long = palabra.length;
+    var sum=0;
+
+    for(var i=0;i<long;i++){
+        sum = parseInt(palabra)+sum;
+    }
+
+    num=sum;
+
     if(palabra == ""){
         num=0;
     }
--- src/calcularCadena.test.js
+++ src/calcularCadena.test.js
@@ -2,9 +2,15 @@ import cadena from "./calcularCad"
 
 describe("Cadena", () => {
     it("al ingresar una cadena de numeros vacia este deberia mostrar un total de 0", () => {
-        expect(cadena("")).toEqual(0);
+        const string = "";
+        expect(cadena(string)).toEqual(0);
     });
     it("cadena con el numero 2 devuelve 2", () => {
-        expect(cadena("2")).toEqual(2);
-    });    
+        const string = "2";
+        expect(cadena(string)).toEqual(2);
+    });
+    it("cadena con dos numeros 1,2 devuelve 3", () => {
+        const string = "1,2";
+        expect(cadena(string)).toEqual(3);
+    });
   });
